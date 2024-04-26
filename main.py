61e19dc55f6808a83fb748f21bd6ef2bb5180e0f
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-27 00:02:50 +0700

    Introduction to the Python continue statement

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 if __name__ == "__main__":
-  print("Python continue")
+  # Introduction to the Python continue statement
+
+  """
+  for index in range(n):
+    if condition:
+      continue
+
+    # more code here
+  """
+
+  """
+  while condition1:
+    if condition2:
+      continue
+
+    # more code here
+  """
